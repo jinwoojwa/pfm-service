ba92b8f744861d5bf0e9e8857d3e1e87cbd19a5a
jinwoojwa <jjw6654@gmail.com>
2025-09-08 17:12:51 +0900

    feat: 프로젝트 초기 환경 설정

---
+++ src/main/java/com/myproject/pfmservice/PfmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.pfmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfmServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/pfmservice/PfmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.pfmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
